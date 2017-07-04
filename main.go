6ac9db5229601cfa39b96eac37985e95e35c060a
jamesallison <jamesallison.uk@gmail.com>
2017-07-05 00:33:12 +0100

    add weather stuff to main.go

--- main.go
+++ main.go
@@ -5,13 +5,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/jamesallison/slack-weather/weather"
 )
 
-var slackWebhook string
+var (
+	slackWebhook string
+	weatherAPIKey string
+)
 
 func main() {
 	port := os.Getenv("PORT")
 	slackWebhook = os.Getenv("SLACKWEBHOOK")
+	weatherAPIKey = os.Getenv("WEATHERAPIKEY")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
@@ -19,6 +25,10 @@ func main() {
 	if slackWebhook == "" {
 		// set to default
 		slackWebhook = "https://hooks.slack.com/services/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"
+	}
+	if weatherAPIKey == "" {
+		// set to default
+		weatherAPIKey = "4a960eda5ddf27ae6b3c6a899d9c5e39"
 	}
 
 	http.HandleFunc("/", rootHandler)
